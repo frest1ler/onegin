2f5a794c776fdfc162f5acfd5dacf45b9dc1a416
frest1ler <143139569+frest1ler@users.noreply.github.com>
2024-09-20 18:38:13 +0300

    Delete interaction_with_files.h

--- interaction_with_files.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTERACTION_WITH_FILES_H
-#define INTERACTION_WITH_FILES_H
-
-#include "onegin.h"
-
-int read_from_file(Text_processing* data, Interaction_files* value);
-int output_text(Interaction_files* value, Text_processing* data);
-
-#endif /*INTERACTION_WITH_FILES_H*/
